e317f9cbef94ab0b60f657d9f77403d259c76f9a
jdeeny <jdeeny@gmail.com>
2019-05-18 17:51:20 -0600

    Game Manager instantiates a spot and a player

---
+++ Input Optional Jam 2019/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject spotPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(spotPrefab, Vector3.zero, Quaternion.identity);
+        Instantiate(playerPrefab, new Vector3(5f, 5f, 0f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
