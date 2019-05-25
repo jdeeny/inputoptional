f79e88bb0c5a76ce1a5df0e103e35f860242f72a
jdeeny <jdeeny@gmail.com>
2019-05-25 14:11:58 -0600

    Add robo footsteps

---
+++ Input Optional Jam 2019/Assets/Scripts/RoboSounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] footstepClips;
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void LeftFootstep()
+    {
+        AudioClip clip = GetRandomFootstepClip();
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void RightFootstep()
+    {
+        LeftFootstep();
+    }
+
+    private AudioClip GetRandomFootstepClip()
+    {
+        return footstepClips[Random.Range(0, footstepClips.Length)];
+    }
+}
